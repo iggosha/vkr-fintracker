84d5aaafbc03e39c128737fb75bae7f0cec434a7
iggosha <iegolovkov@gmail.com>
2025-04-27 16:20:20 +0300

    Создание проекта

---
+++ src/test/java/ru/golovkov/fintracker/FintrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.golovkov.fintracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
