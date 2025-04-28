9006d9dbaea09fd62156bbacccb59e80a01c260b
iggosha <iegolovkov@gmail.com>
2025-04-29 01:30:04 +0300

    Реализованы необходимые CRUD-методы во всех контроллерах

--- src/main/java/ru/golovkov/fintracker/controller/AccountController.java
+++ src/main/java/ru/golovkov/fintracker/controller/AccountController.java
@@ -1,14 +1,50 @@
 package ru.golovkov.fintracker.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.golovkov.fintracker.dto.AccountDto;
 import ru.golovkov.fintracker.service.AccountService;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/accounts")
 public class AccountController {
 
-    private final AccountService accountService;
+    private final AccountService service;
+
+    @PostMapping("/clients/{clientId}/accounts")
+    public AccountDto create(@PathVariable UUID clientId) {
+        return service.create(clientId);
+    }
+
+    @GetMapping("/accounts")
+    public List<AccountDto> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/clients/{clientId}/accounts")
+    public List<AccountDto> getAllByClientId(@PathVariable UUID clientId) {
+        return service.getAllByClientId(clientId);
+    }
+
+    @GetMapping("/accounts/{id}")
+    public AccountDto getById(@PathVariable String id) {
+        return service.getById(id);
+    }
+
+    @DeleteMapping("/accounts/{id}")
+    public void deleteById(@PathVariable String id) {
+        service.deleteById(id);
+    }
+
+    @DeleteMapping("/clients/{clientId}/accounts")
+    public void deleteAllByClientId(@PathVariable UUID clientId) {
+        service.deleteAllByClientId(clientId);
+    }
 }
--- src/main/java/ru/golovkov/fintracker/controller/MoneyFlowController.java
+++ src/main/java/ru/golovkov/fintracker/controller/MoneyFlowController.java
@@ -3,12 +3,15 @@ package ru.golovkov.fintracker.controller;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,23 +19,59 @@ import ru.golovkov.fintracker.dto.MoneyFlowDto;
 import ru.golovkov.fintracker.service.MoneyFlowService;
 
 import java.util.List;
+import java.util.UUID;
+
+import static org.springframework.data.domain.Sort.Direction.DESC;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/flows")
 public class MoneyFlowController {
 
     private final MoneyFlowService service;
 
-    @PostMapping(value = "/parsed-entities", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/flows/parsed-entities", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public void parse(@RequestPart MultipartFile sheetFile) {
         service.createFromFile(sheetFile);
     }
 
-    @GetMapping
-    public List<MoneyFlowDto> getAll(
-            @ParameterObject @PageableDefault(sort = {"date"}, direction = Sort.Direction.DESC) Pageable pageable
+    @PostMapping("/flows")
+    public MoneyFlowDto create(@RequestBody MoneyFlowDto moneyFlowDto, @RequestParam String accountId) {
+        return service.create(moneyFlowDto, accountId);
+    }
+
+    @GetMapping("/clients/{clientId}/flows")
+    public List<MoneyFlowDto> getAllByClientId(
+            @PathVariable UUID clientId,
+            @ParameterObject @PageableDefault(sort = {"date"}, direction = DESC) Pageable pageable
     ) {
-        return service.getAll(pageable);
+        return service.getAllByClientId(clientId, pageable);
+    }
+
+    @GetMapping("/accounts/{accountId}/flows")
+    public List<MoneyFlowDto> getAllByAccountId(
+            @PathVariable String accountId,
+            @ParameterObject @PageableDefault(sort = {"date"}, direction = DESC) Pageable pageable
+    ) {
+        return service.getAllByAccountId(accountId, pageable);
+    }
+
+    @GetMapping("/flows/{id}")
+    public MoneyFlowDto getById(@PathVariable String id) {
+        return service.getById(id);
+    }
+
+    @PutMapping("/flows/{id}")
+    public MoneyFlowDto update(@PathVariable String id, @RequestBody MoneyFlowDto moneyFlowDto) {
+        return service.updateById(id, moneyFlowDto);
+    }
+
+    @DeleteMapping("/flows/{id}")
+    public void deleteById(@PathVariable String id) {
+        service.deleteById(id);
+    }
+
+    @DeleteMapping("/flows")
+    public void deleteAll() {
+        service.deleteAll();
     }
 }
